a803666e7055f36eefedb118d4dda5e65ebf2cfe
Rafael <rafael.berger20@gmail.com>
2021-12-13 21:06:18 -0300

    Feito countdown para uma data

---
+++ javascript.js
@@ -0,0 +1,34 @@
+const segundosNaTela = document.querySelector("#segundos");
+const minutosNaTela = document.querySelector("#minutos");
+const horasNaTela = document.querySelector("#horas");
+const diasNaTela = document.querySelector("#dias");
+
+let temaDaContagem = document.querySelector("#tema");
+temaDaContagem.innerHTML = "Ano novo";
+
+function countdown() {
+  const anoNovo = new Date(2022, 0, 1);
+  const dataAtual = new Date();
+
+  const segundosTotal = Math.floor((anoNovo - dataAtual) / 1000);
+
+  const segundos = Math.floor(segundosTotal % 60);
+  const minutos = Math.floor(segundosTotal / 60) % 60;
+  const horas = Math.floor(segundosTotal / 3600) % 60;
+  const dias = Math.floor(segundosTotal / 3600 / 24);
+
+  segundosNaTela.innerHTML = converteTempo(segundos);
+  minutosNaTela.innerHTML = converteTempo(minutos);
+  horasNaTela.innerHTML = converteTempo(horas);
+  diasNaTela.innerHTML = converteTempo(dias);
+}
+
+function converteTempo(tempo) {
+  if (tempo < 10) {
+    return `0${tempo}`;
+  } else {
+    return tempo;
+  }
+}
+
+setInterval(countdown, 1000);
